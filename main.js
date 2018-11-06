1bd2cd113c7a09e4eb51045c76e6caa1fe890799
Olivia Ellis <oaksellis@gmail.com>
2018-11-06 14:28:40 -0500

    Pre-workshop JS (half working)

--- main.js
+++ main.js
@@ -1,31 +1,27 @@
-var newPoke = ['lickitung', 'rowlet', 'exeggutor'];
-
-// NEXT STEP: Figure out how to loop over all Pokemon to add them all to the trainer object.
-for (i = 0; i < newPoke.length; i++) {
-var httpRequest = new XMLHttpRequest();
-httpRequest.onreadystatechange = addPokemon;
-httpRequest.open('GET', 'http://fizal.me/pokeapi/api/v2/name/' + newPoke[i] + '.json');
-httpRequest.send();
-}
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = addPokemon;
+xhttp.open('GET', 'http://fizal.me/pokeapi/api/v2/name/lickitung.json', true);
+xhttp.send();
 
 function addPokemon() {
-if (httpRequest.readyState === XMLHttpRequest.DONE) {
-  if (httpRequest.status === 200) {
-    var info = JSON.parse(httpRequest.responseText);
-    var pokeName = info['forms'][0]['name'];
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    var info = JSON.parse(xhttp.responseText);
+    var pokeName = info['name'];
     var hp = info['stats'][5]['base_stat'];
     var attack = info['stats'][4]['base_stat'];
     var defense = info['stats'][3]['base_stat']
     var abilities = [];
     ability1 = info['abilities'][0]['ability']['name'];
-    abilities.push(ability1);
-    new Pokemon(pokeName, hp, attack, defense, abilities);
-    // console.log(info);
+    ability2 = info['abilities'][1]['ability']['name'];
+    abilities.push(ability1, ability2);
+    let pokemon = new Pokemon(pokeName, hp, attack, defense, abilities);
   } else {
     console.log('There was an error retrieving your request.');
+    console.log(xhttp.readyState);
+    console.log(xhttp.status);
   }
 }
-}
+
 
 class Trainer {
   constructor(name) {
@@ -52,7 +48,7 @@ class Pokemon {
     this.hp = hp;
     this.attack = attack;
     this.defense = defense;
-    this.abilities = [];
+    this.abilities = abilities;
     trainer.team.push(this);
     }
 }
